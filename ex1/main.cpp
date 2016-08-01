cbca4b4f7e0ed63bdc1259d23eb9f90cf81e6ea3
Yury Chebiryak <yury@chebiryak.name>
2016-08-01 23:08:13 +0200

    second solution uses additional data structure: string. Is that still allowed? We also could do in-place sorting

--- ex1/main.cpp
+++ ex1/main.cpp
@@ -5,14 +5,18 @@
 //  Created by Yury Chebiryak on 01/08/16.
 //  Copyright © 2016 Yury Chebiryak. All rights reserved.
 //
-
+#include <algorithm>
 #include <iostream>
-
+//#include <boost/flat_set>
 #include <set>
 // Implement an algorithm to determine if a string has all unique character.
 // What if you cannot use additional data structures?
 
-bool naiveSolution(const std::string in) {
+// quick solution using std::set
+//   we insert characters we've seen already into the set
+//   once we encounter a character we haven't seen already => not unique chars
+// complexity: going thru all characters in string is linear, inserting is log n, thus \bigO(n log n)
+bool naiveSolution(const std::string& in) {
     std::set<char> visited;
     for (char c : in)
         if (visited.find(c) == visited.end())
@@ -21,6 +25,26 @@ bool naiveSolution(const std::string in) {
             return false;
     return true;
 }
+
+//if we can't use additional data structures, we still can simply sort the string,
+// and then go through it and figure out if there are non-unique characters
+// for the same complexity: sorting is O(n log n) + linear pass-through = O(n log n)
+bool uniqueChars(const std::string& in) {
+    std::string sorted { in};
+    std::sort(sorted.begin(), sorted.end());
+    char prevChar = '\0';
+    for (char c : sorted)
+        if (prevChar == '\0') {
+            prevChar = c;
+            continue;
+        } else {
+            if (c == prevChar)
+                return false;
+            prevChar = c;
+        }
+    return true;
+}
+
 
 int main(int argc, const char * argv[]) {
     std::string testing="aafqnvz";

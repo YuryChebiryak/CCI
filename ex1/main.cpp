9cdc43c6bd34ec28ce18f5c39cc1b0df47b4b72b
Yury Chebiryak <yury@chebiryak.name>
2016-08-01 22:33:40 +0200

    Initial Commit

---
+++ ex1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ex1
+//
+//  Created by Yury Chebiryak on 01/08/16.
+//  Copyright © 2016 Yury Chebiryak. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

707ff626ca17f77b175461815c12f17f71d857de
Yury Chebiryak <chebiryak@gmail.com>
2016-08-19 16:58:04 +0200

    length check

--- chapter1/ex1.3/ex1.3/main.cpp
+++ chapter1/ex1.3/ex1.3/main.cpp
@@ -15,6 +15,8 @@ using namespace std;
 //! in case strings are very long, this solves the task in O(n)
 //  We compute the number of each character in both strings and compare the character counts
 bool isPermutationNaive(const std::string& a, const std::string& b) {
+    if (a.length() != b.length())
+        return false;
     vector<int> chars;
     chars.assign(32, 0);
     for (auto i = 0; i < a.length(); ++i)
@@ -33,6 +35,8 @@ bool isPermutationNaive(const std::string& a, const std::string& b) {
 //! in case strings aren't too long, this solves the task in O(n log n)
 //  We sort both strings and compare the results, if its identical one is a permutation of another
 bool isPermutationSort(const string& a, const string& b) {
+    if (a.length() != b.length())
+        return false;
     string asorted {a}, bsorted {b};
     sort(asorted.begin(), asorted.end());
     sort(bsorted.begin(), bsorted.end());
@@ -40,10 +44,12 @@ bool isPermutationSort(const string& a, const string& b) {
 }
 
 int main(int argc, const char * argv[]) {
-    string a = "apple", b1 = "pplea", b2 = "pple";
+    string a = "apple", b1 = "pplea", b2 = "pple", b3 {"bpple"};
     cout << a << " perm of " << b1 << "? " << isPermutationNaive(a, b1) << endl;
     cout << a << " perm of " << b2 << "? " << isPermutationNaive(a, b2) << endl;
+    cout << a << " perm of " << b3 << "? " << isPermutationNaive(a, b3) << endl;
     cout << a << " perm of " << b1 << "? " << isPermutationSort(a, b1) << endl;
     cout << a << " perm of " << b2 << "? " << isPermutationSort(a, b2) << endl;
+    cout << a << " perm of " << b3 << "? " << isPermutationSort(a, b3) << endl;
     return 0;
 }

e24ee83efb3f76d9fce21a009f71a67d4a1963f3
Yury Chebiryak <yury@chebiryak.name>
2016-08-19 18:36:04 +0200

    replaced the for loop by a for_each and a lambda

--- chapter1/ex1.3/ex1.3/main.cpp
+++ chapter1/ex1.3/ex1.3/main.cpp
@@ -5,10 +5,11 @@
 //  Created by Yury Chebiryak on 19/08/16.
 //  Copyright © 2016 Yury Chebiryak. All rights reserved.
 //
-
+#include <string>
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <functional>
 // 1.3 Given two strings, write a method to decide if one is a permutation of the other.
 using namespace std;
 
@@ -19,16 +20,14 @@ bool isPermutationNaive(const std::string& a, const std::string& b) {
         return false;
     vector<int> chars;
     chars.assign(32, 0);
-    for (auto i = 0; i < a.length(); ++i)
-        chars[ a[i] - 'a' ]++;
+	for_each(a.begin(), a.end(), [&chars](char c) {chars[c - 'a']++; });
+    //for (auto i = 0; i < a.length(); ++i)
+    //    chars[ a[i] - 'a' ]++;
     for (auto i = 0; i < b.length(); ++i) {
         chars[ b[i] - 'a' ]--;
         if (chars[ b[i] - 'a'] < 0)
             return false;
     }
-    for (auto count: chars)
-        if (count != 0)
-            return false;
     return true;
 }
 

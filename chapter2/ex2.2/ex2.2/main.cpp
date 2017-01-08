db145103b582da3bde4ba530644ddbcc02d3a74a
Yury Chebiryak <chebiryak@gmail.com>
2017-01-08 20:50:13 +0100

    fix

--- chapter2/ex2.2/ex2.2/main.cpp
+++ chapter2/ex2.2/ex2.2/main.cpp
@@ -9,6 +9,8 @@
 #include <iostream>
 #include <algorithm>
 #include <forward_list>
+
+//// Cracking the Coding Interview, 5th Edition
 //2.2 Implement an algorithm to find the kth to last element of a singly linked list.
 
 template<typename T>

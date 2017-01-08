d1e6fa17d7740790f1db539198261f4c089cf900
Yury Chebiryak <chebiryak@gmail.com>
2017-01-08 20:53:13 +0100

    fixes

--- chapter1/ex1.5/ex1.5/main.cpp
+++ chapter1/ex1.5/ex1.5/main.cpp
@@ -7,7 +7,7 @@
 //
 
 #include <iostream>
-
+// Cracking the Coding Interview, 5th Edition
 //1.5 Implement a method to perform basic string compression using the counts of repeated characters. For example, the string a a b c c c c c a a a would become a2blc5a3. If the "compressed" string would not become smaller than the original string, your method should return the original string
 #include <string>
 #include <stdlib.h>

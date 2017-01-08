89c940783fd852deb4cbf393009939cb72fc5586
Yury Chebiryak <chebiryak@gmail.com>
2017-01-08 20:53:42 +0100

    files

---
+++ chapter2/ex2.6/ex2.6/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ex2.6
+//
+//  Created by Yury Chebiryak on 06/01/17.
+//  Copyright © 2017 Yury Chebiryak. All rights reserved.
+//
+
+#include <iostream>
+
+// Cracking the coding interview, 5th Edition
+//2.6 Given a circular linked list, implement an algorithm which returns the node at the beginning of the loop.
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
